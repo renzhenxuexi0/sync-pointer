4b5ef4a0a250d1e75e2c77dd4b3dcdf55fe7ef0e
wuhaiyue <3084972826@qq.com>
2025-02-10 10:18:07 +0800

    feat: 更新VSCode设置，优化Rust格式化配置，调整日志时间格式

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,7 +2,7 @@ pub mod client;
 pub mod config;
 pub mod server;
 
-use chrono::Utc;
+use chrono::Local;
 use tauri_plugin_log::{Target, TargetKind};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -26,16 +26,14 @@ pub fn run() {
                 .format(move |out, message, record| {
                     out.finish(format_args!(
                         "[[{date}]-[{level}]-[{target}]] {message}",
-                        date = Utc::now().format("%Y-%m-%d %H:%M:%S%.3f"),
+                        date = Local::now().format("%Y-%m-%d %H:%M:%S%.3f"),
                         level = record.level(),
                         target = record.target(),
                         message = message
                     ))
                 })
                 // Add blanket level filter -
-                .level(log::LevelFilter::Debug)
-                // - and per-module overrides
-                .level_for("hyper", log::LevelFilter::Info)
+                .level(log::LevelFilter::Info)
                 .build(),
         )
         .plugin(tauri_plugin_valtio::init())
